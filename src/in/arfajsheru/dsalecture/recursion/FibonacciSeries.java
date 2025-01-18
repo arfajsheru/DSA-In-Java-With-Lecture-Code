9f075de9893987f937bec57a53350b7d2da71c2a
arfajsheru <arfatsheru74@gmail.com>
2025-01-18 11:19:48 +0530

    Recursive lecure fibonacci series added

--- src/in/arfajsheru/dsalecture/recursion/FibonacciSeries.java
+++ src/in/arfajsheru/dsalecture/recursion/FibonacciSeries.java
@@ -2,10 +2,19 @@ package in.arfajsheru.dsalecture.recursion;
 
 public class FibonacciSeries {
     public static void main(String[] args) {
-        
+        int terms = 10;
+        for(int i = 0; i < terms; i++){
+            System.out.print(findFibonacci(i) + " ");
+        }
     }
 
     public static int findFibonacci(int number){
-        
+     if(number == 0){
+        return 0;
+     }
+     if(number == 1){
+        return 1;
+     }
+     return findFibonacci(number - 1) + findFibonacci(number - 2);
     }
 }
